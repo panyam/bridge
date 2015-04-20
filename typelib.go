77f01786c778743e555f7e1e91f4d2f173bc6972
Sriram Panyam <sri.panyam@gmail.com>
2015-04-20 01:43:45 -0700

    Adding basic types

--- typelib.go
+++ typelib.go
@@ -26,7 +26,47 @@ func NewTypeLibrary() *TypeLibrary {
 	out.pkgByShortName = make(map[string]string)
 	out.shortNamesForPkg = make(map[string]string)
 	out.types = make(map[string]*Type)
+
+	// add some basic types
+	out.AddBasicType("string")
+	out.AddBasicType("float")
+	out.AddBasicType("float32")
+	out.AddBasicType("float64")
+	out.AddBasicType("bool")
+	out.AddBasicType("byte")
+	out.AddBasicType("int")
+	out.AddBasicType("int16")
+	out.AddBasicType("int32")
+	out.AddBasicType("int64")
+	out.AddBasicType("uint")
+	out.AddBasicType("uint16")
+	out.AddBasicType("uint32")
+	out.AddBasicType("uint64")
 	return &out
+}
+
+func (tl *TypeLibrary) AddBasicType(name string) (alt *Type) {
+	return tl.AddType("", name, &Type{BasicType, name})
+}
+
+/**
+ * Adds a type to the type system.  If the type already exists then
+ * the existing one is returned otherwise a new type is added and returned.
+ * Also the type's ID will be set.
+ */
+func (tl *TypeLibrary) AddType(pkg string, name string, t *Type) (alt *Type) {
+	key := pkg + "." + name
+	if value, ok := tl.types[key]; ok {
+		return value
+	}
+	tl.typeCounter++
+	tl.types[key] = t
+	return t
+}
+
+func (tl *TypeLibrary) GetType(pkg string, name string) *Type {
+	key := pkg + "." + name
+	return tl.types[key]
 }
 
 func (tl *TypeLibrary) GetShortPackageName(pkg string) string {
@@ -51,28 +91,4 @@ func (tl *TypeLibrary) GetShortPackageName(pkg string) string {
 	tl.shortNamesForPkg[name] = pkg
 	tl.pkgByShortName[pkg] = name
 	return name
-}
-
-/**
- * Adds a type to the type system.  If the type already exists then
- * the existing one is returned otherwise a new type is added and returned.
- * Also the type's ID will be set.
- */
-func (tl *TypeLibrary) AddType(pkg string, name string, t *Type) (alt *Type) {
-	key := pkg + "." + name
-	if value, ok := tl.types[key]; ok {
-		return value
-	}
-	tl.typeCounter++
-	tl.types[key] = t
-	return t
-}
-
-func (tl *TypeLibrary) GetType(pkg string, name string) *Type {
-	key := pkg + "." + name
-	return tl.types[key]
-}
-
-func (tl *TypeLibrary) FindType(typeClass int, typeData interface{}) string {
-	return ""
 }
