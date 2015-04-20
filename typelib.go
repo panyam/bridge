3148ac9f7b3902b2ab01909f3bb00dc4bad2807f
Sriram Panyam <sri.panyam@gmail.com>
2015-04-20 01:37:29 -0700

    Adding method to generate local package names to be used durign importing

--- typelib.go
+++ typelib.go
@@ -1,24 +1,56 @@
 package bridge
 
 import (
+	"fmt"
 	"log"
+	"path/filepath"
 )
 
 type ITypeLibrary interface {
 	AddType(pkg string, name string, t *Type) (alt *Type)
 	GetType(pkg string, name string) *Type
+	GetShortPackageName(pkg string) string
 }
 
 type TypeLibrary struct {
-	types       map[string]*Type
-	typeCounter int64
+	shortNamesForPkg map[string]string
+	pkgByShortName   map[string]string
+	types            map[string]*Type
+	typeCounter      int64
+	pkgCounter       int
 }
 
 func NewTypeLibrary() *TypeLibrary {
-	log.Println("Creating new type system... ", pkg)
+	log.Println("Creating new type library... ")
 	out := TypeLibrary{}
+	out.pkgByShortName = make(map[string]string)
+	out.shortNamesForPkg = make(map[string]string)
 	out.types = make(map[string]*Type)
 	return &out
+}
+
+func (tl *TypeLibrary) GetShortPackageName(pkg string) string {
+	if value, ok := tl.shortNamesForPkg[pkg]; ok {
+		return value
+	}
+
+	// create a name and return it (and save it ofcourse)
+	name := ""
+	for pkg != "" {
+		name := filepath.Base(pkg) + name
+		if _, ok := tl.shortNamesForPkg[pkg]; !ok {
+			tl.shortNamesForPkg[name] = pkg
+			tl.pkgByShortName[pkg] = name
+			return name
+		}
+		pkg = filepath.Dir(pkg)
+	}
+
+	// return a random name!
+	name = fmt.Sprintf("pkg%d", tl.pkgCounter)
+	tl.shortNamesForPkg[name] = pkg
+	tl.pkgByShortName[pkg] = name
+	return name
 }
 
 /**
@@ -26,21 +58,21 @@ func NewTypeLibrary() *TypeLibrary {
  * the existing one is returned otherwise a new type is added and returned.
  * Also the type's ID will be set.
  */
-func (ts *TypeLibrary) AddType(pkg string, name string, t *Type) (alt *Type) {
+func (tl *TypeLibrary) AddType(pkg string, name string, t *Type) (alt *Type) {
 	key := pkg + "." + name
-	if value, ok := ts.types[key]; ok {
+	if value, ok := tl.types[key]; ok {
 		return value
 	}
-	ts.typeCounter++
-	ts.types[key] = t
+	tl.typeCounter++
+	tl.types[key] = t
 	return t
 }
 
-func (ts *TypeLibrary) GetType(pkg string, name string) *Type {
+func (tl *TypeLibrary) GetType(pkg string, name string) *Type {
 	key := pkg + "." + name
-	return ts.types[key]
+	return tl.types[key]
 }
 
-func (ts *TypeLibrary) FindType(typeClass int, typeData interface{}) string {
+func (tl *TypeLibrary) FindType(typeClass int, typeData interface{}) string {
 	return ""
 }
